f679a13d2a976211e0977ff82dcc7d18da967159
anurag270102 <anuragdalsaniya@gmail.com>
2023-12-14 09:38:27 +0530

    add footer

--- src/component/project/Project.jsx
+++ src/component/project/Project.jsx
@@ -1,20 +1,8 @@
 const Project = () => {
     return ([
         <div key={1212}>
-            <h2 className="font-PlayfairDisplaySC font-bold mx-auto items-center text-center text-black text-4xl mt-5 bg-white">Projects</h2>
-            <div className="grid max-w-screen-lg mx-auto">
-      <div className="flex flex-wrap -mx-4">
-        <div className="w-1/3 px-4">
-          <div className="box bg-gray-300 relative overflow-hidden hover:bg-green-500 transition-all duration-500">
-            <span className="original text-white">
-              <h1>Hover!</h1>
-              <p>Slide in from left</p>
-            </span>
-          </div>
-        </div>
-        {/* ... other grid items ... */}
-      </div>
-    </div>
+          <h2 className="font-PlayfairDisplaySC font-bold mx-auto items-center text-center text-black text-4xl mt-5 ">Projects</h2>
+            
         </div>
     ])
 }
